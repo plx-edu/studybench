9ff00f284a20d6f9dd9a9b798d1103de2bfae1de
xac <xac@pop-os.localdomain>
2022-01-12 09:41:33 +0100

    Added little test soon to be modified

--- workbench.js
+++ workbench.js
@@ -2,15 +2,7 @@
 /////////////////////// UTILITIES ////////////////////////////////
 //////////////////////////////////////////////////////////////////
 let smbl = ":::::";
-const print = (...arg) => {
-    let toPrint = "";
-    for (const k of arg) {
-        toPrint += k;
-        if (arg.indexOf(k) < (arg.length - 1)) toPrint += " ";
-    }
-    console.log(toPrint);
-};
-const br = () => { print("\n"); };
+const br = () => { console.log("\n"); };
 const tab = (n = Number = 0) => { return "\t".repeat(n); };
 
 
@@ -30,18 +22,18 @@ function showLesson(num) {
         default:
             // callbackStudy();
             // br();
-            print("##### Start of Studies #####");
+            console.log("##### Start of Studies #####");
             promiseStudy();
             br();
             callbackStudy();
             br();
             asyncAwaitStudy();
-            print("##### End studies #####");
+            console.log("##### End studies #####");
             br();
             break;
     }
 }
-showLesson(3);
+// showLesson(3);
 
 /////////////////////////////////////////////////
 /////////// DISSECTION OF A CALLBACK ////////////
@@ -52,28 +44,28 @@ function callbackStudy() {
     let _t = tab();
 
     // A
-    print(smbl + " Dissection of a callback " + smbl);
-    print(`(A) Init - light value: OFF`);
+    console.log(smbl + " Dissection of a callback " + smbl);
+    console.log(`(A) Init - light value: OFF`);
     // B
     function toggle(_val) {
-        print(`${_t}(B) toggle() called`);
+        console.log(`${_t}(B) toggle() called`);
         return _val ? false : true;
     }
     // C
     function turnOnOff(arg, callback) {
-        print(`${_t}(C) turnOnOff() called`);
+        console.log(`${_t}(C) turnOnOff() called`);
         isLightOn = callback(arg);
         return isLightOn ? "Light is ON" : "Light is OFF";
     };
     // D
-    print(`${_t}(D) switchLight() called (x2)`);
+    console.log(`${_t}(D) switchLight() called (x2)`);
     for (let i = 0; i < 2; i++) {
         _t = tab(1) + `(${i + 1})`;
-        print(`${_t + tab(1) + (i + 1)})`, switchLight(isLightOn, toggle));
+        console.log(`${_t + tab(1) + (i + 1)})`, switchLight(isLightOn, toggle));
     }
     // E
-    print(`(E) End of Lesson\n`);
-    print(smbl + " Dissection of a callback " + smbl);
+    console.log(`(E) End of Lesson\n`);
+    console.log(smbl + " Dissection of a callback " + smbl);
 
 }// callbackStudy()
 
@@ -84,26 +76,26 @@ function promiseStudy() {
     let countCalls = 0; // used to check number of calls in promise
 
     // A
-    print(smbl + " Dissection of a promise " + smbl);
-    print(`(A) Init - successful result = true`);
+    console.log(smbl + " Dissection of a promise " + smbl);
+    console.log(`(A) Init - successful result = true`);
 
     // B
     // function passed to new Promise is called the executor
     // executor runs automatically and immediately on promise creation
     // --> Callbacks
-    print(`(B) promise is created`);
+    console.log(`(B) promise is created`);
     let promise = new Promise(myExecutor);
     // B-1
-    print(`(B-1) promise.then() is called`);
+    console.log(`(B-1) promise.then() is called`);
     promise.then(
-        result => print(tab(2) + `(B-1.1)` + result),
-        error => print(tab(2) + `(B-1.1)` + error));
+        result => console.log(tab(2) + `(B-1.1)` + result),
+        error => console.log(tab(2) + `(B-1.1)` + error));
 
 
     // C
     // resolve and reject are provided vy
     function myExecutor(myResolve, myReject) {
-        print(`(C) executor is called`);
+        console.log(`(C) executor is called`);
 
         if (thisIsOver()) {
             myResolve(success());
@@ -113,40 +105,40 @@ function promiseStudy() {
     }
     // C-1 - function when "resolve"
     function success() {
-        print(tab(1) + `(C-1.${++countCalls}) resolve runs function passed to it`);
+        console.log(tab(1) + `(C-1.${++countCalls}) resolve runs function passed to it`);
         return `(C-1.${++countCalls}) result of resolved`;
     }
     // C-2 - function when "reject"
     function failure() {
-        print(tab(1) + `(C-2.${++countCalls}) rejected runs function passed to it`);
+        console.log(tab(1) + `(C-2.${++countCalls}) rejected runs function passed to it`);
         return `(C-2.${++countCalls}) result of rejected`;
     }
 
     // D
     function thisIsOver() {
-        print(`(D) job that has to finish for promise outcome`);
+        console.log(`(D) job that has to finish for promise outcome`);
         return successful;
     }
 
     // E
-    print(`(E) promise.then() is called again with chaining`);
+    console.log(`(E) promise.then() is called again with chaining`);
     successful = false;
     promise.then(
-        result => print(tab(1) + `(E-1)` + result),
-        error => print(tab(1) + `(E-1)` + error))
-        .then(() => print(tab(2) + `(E-2) chained .then() based on result`))
-        .then(print(tab(3) + `(E-3) chained .then() NOT based on result`));
+        result => console.log(tab(1) + `(E-1)` + result),
+        error => console.log(tab(1) + `(E-1)` + error))
+        .then(() => console.log(tab(2) + `(E-2) chained .then() based on result`))
+        .then(console.log(tab(3) + `(E-3) chained .then() NOT based on result`));
 
 
     // F
-    print(`(F) promise.then() is called with success function as callback`);
-    print(`   you'll see another (C-1) amongst the other results`);
-    print(`   notice how it's not the (C-1.x) that the function returns`);
+    console.log(`(F) promise.then() is called with success function as callback`);
+    console.log(`   you'll see another (C-1) amongst the other results`);
+    console.log(`   notice how it's not the (C-1.x) that the function returns`);
     promise.then(success);
 
     // G
-    print(`(G) End of Lesson (or is it ?)`);
-    print(smbl + " Dissection of a promise " + smbl);
+    console.log(`(G) End of Lesson (or is it ?)`);
+    console.log(smbl + " Dissection of a promise " + smbl);
 
 }// promiseStudy()
 
@@ -156,62 +148,162 @@ function asyncAwaitStudy() {
     let countCalls = 0;
 
     // A
-    print(smbl + " Dissection of async/await " + smbl);
-    print(`(A) Init`);
+    console.log(smbl + " Dissection of async/await " + smbl);
+    console.log(`(A) Init`);
 
 
     // B
     // async functions always returns a promise.
-    print(`(B) "outer" async function definition`);
+    console.log(`(B) "outer" async function definition`);
     async function asyncParent() {
         // B-1
-        print(tab(1) + `(B-${++countCalls}) "inner" is running`);
+        console.log(tab(1) + `(B-${++countCalls}) "inner" is running`);
 
         // B-2
-        print(tab(1) + `(B-${++countCalls}) "inner", defining a promise through the async syntax`);
+        console.log(tab(1) + `(B-${++countCalls}) "inner", defining a promise through the async syntax`);
         async function asyncChild() {
-            print(tab(2) + `(B-${++countCalls}) "inner async" is running`);
+            console.log(tab(2) + `(B-${++countCalls}) "inner async" is running`);
             return `(B-${++countCalls}) SUCCESS (inner)`;
         }
 
         // B-3
-        print(tab(1) + `(B-${++countCalls}) using await through variable affectation`);
+        console.log(tab(1) + `(B-${++countCalls}) using await through variable affectation`);
         let result = await asyncChild();
-        print(tab(1) + `(B-${++countCalls})` + result);
+        console.log(tab(1) + `(B-${++countCalls})` + result);
 
         // something "nice" happens when duplicated
         // uncomment next line to see (and copy as many times as you want)
-        // print(tab(1) + `(B-${++countCalls})` + result);
+        // console.log(tab(1) + `(B-${++countCalls})` + result);
 
         // B-7
-        print(tab(1) + `(B-${++countCalls}) using await "directly" with a then`);
-        print(tab(2) + `(B-${++countCalls})` + await asyncChild()
-            .then(print(tab(3) + `(B-${++countCalls}).then ` + await asyncChild())));
+        console.log(tab(1) + `(B-${++countCalls}) using await "directly" with a then`);
+        console.log(tab(2) + `(B-${++countCalls})` + await asyncChild()
+            .then(console.log(tab(3) + `(B-${++countCalls}).then ` + await asyncChild())));
 
 
         return `(B-${++countCalls}) SUCCESS`;
     }
 
     // C
-    // print(`(C) calling async function with .then()`);
+    // console.log(`(C) calling async function with .then()`);
     // console.log(asyncParent().then(
-    //     result => print(tab(1) + `(C-1) result: ` + result)));
+    //     result => console.log(tab(1) + `(C-1) result: ` + result)));
 
-    print(`(C) calling async function WITHOUT .then()`);
+    console.log(`(C) calling async function WITHOUT .then()`);
     console.log(asyncParent());
-    print(`(D) ^^^ Promise object above is async function called at "C"`);
+    console.log(`(D) ^^^ Promise object above is async function called at "C"`);
 
 
 
     // (E)
-    print(`(E) End of Lesson (or is it ?)`);
-    print(smbl + " Dissection of async/await " + smbl);
+    console.log(`(E) End of Lesson (or is it ?)`);
+    console.log(smbl + " Dissection of async/await " + smbl);
 
 }// asyncAwaitStudy()
 
 
+/////////////////////////////////////////////////
+/////////////// TEMPORARY TESTING  //////////////
+
+function tempTest() {
+
+    const getMainTag = () => { return document.getElementsByTagName("main"); };
+    const numberOfPlayers = 4;
+    const listOfPlayers = createPlayers(numberOfPlayers);
+    const playerActions = {};
+    let callCount = 0;
 
 
+    for (const k of listOfPlayers) {
+        let bttn = document.createElement("button");
+        bttn.style.padding = "5px";
+        bttn.id = k;
+        bttn.classList.add("pBttn");
+        bttn.innerText = k;
+        bttn.addEventListener("click", hasClicked);
+        // bttn.addEventListener("click", allHavePlayed);
+        bttn.addEventListener("click", bttnAwait);
+
+        getMainTag()[0].append(bttn);
+    }
+
+    function hasClicked(e) {
+        const targ = e.target.id;
+        callCount = 0;
+
+        if (playerActions.hasOwnProperty(targ)) return;
+
+        playerActions[targ] = true;
+    }
+    console.log(playerActions);
+
+    async function allHavePlayed() {
+
+        console.log(tab(1) + `${++callCount}: Start of allHavePlayed()`);
+        async function confirmAllPlayed(obj) {
+            console.log(tab(2) + `${++callCount}: Start of confirmAllPlayed()`);
+            let n = 0;
+
+            for (const k in obj) {
+                n = obj[k] ? ++n : n;
+                document.getElementById(k).disabled = true;
+                // console.log("::", n);
+            }
+
+
+            return (n === numberOfPlayers) ? true : false;
+            //if (n === numberOfPlayers)
+            // return "CONFIRMED";
+        }
+
+        console.log(tab(1) + `${++callCount}: Await of confirmAllPlayed()`);
+        // await confirmAllPlayed(playerActions).then();
+        let result = await confirmAllPlayed(playerActions);
+        console.log(`${++callCount}: Consuming(?) await confirmAllPlayed(): ` + result);
+
+        return await confirmAllPlayed(playerActions).then(
+            result => isMonsterTurn(result));
+    }
+
+    // console.log(`${++callCount}: Call of confirmAllPlayed()`);
+    console.log(`A: Call of confirmAllPlayed()`);
+    console.log("::: " + allHavePlayed()
+        .then(
+            result => console.log("::: " + result + " SUCCESS !")),
+        error => new Error);
+
+    function bttnAwait() {
+        allHavePlayed().then(
+            result => console.log("#:#:# ", result));
+    }
+
+    function isMonsterTurn(val) {
+        if (val) {
+            console.log("Monster is attacking!");
+            for (const k of listOfPlayers) {
+                document.getElementById(k).disabled = false;
+            }
+        }
+    }
+
+
+
+}// tempTest() {
+
+
+tempTest();
+doSomething();
+
+function doSomething() { console.log("Doing Something"); }
+
+function createPlayers(n = Number = 0) {
+    let p = []; //["P1", "P2", "P3"]
+    for (let i = 0; i < n; i++) p.push("P" + (i + 1));
+    return p;
+}
+
+// Leads to study ?
+// - new MutationObserver
 
 
 
